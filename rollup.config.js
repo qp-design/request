2423458d7bc45493764482bd3c7b251c9d7268fe
devil <devilchinaqin@gmail.com>
2023-11-06 20:39:45 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import ts from 'rollup-plugin-typescript2';
 import terser from '@rollup/plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
+// import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
@@ -12,11 +12,11 @@ export default {
   ],
   external: ['lodash-es', 'qs', '@brushes/utils', 'flyio/src/adapter/wx', 'flyio/dist/npm/engine-wrapper', 'flyio/dist/npm/fly'],
   plugins: [
-    commonjs({
-      exclude: 'node_modules/**',
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
-      transformMixedEsModules: true
-    }),
+    // commonjs({
+    //   exclude: 'node_modules/**',
+    //   include: ['src/**/*.ts', 'src/**/*.tsx'],
+    //   transformMixedEsModules: true
+    // }),
     ts({}),
     terser(),
   ]

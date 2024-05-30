01cd039d7c4a1366bc2c546e2107bdea40a4f66a
秦 <qin@qindeMacBook-Pro.local>
2024-05-30 22:06:52 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,17 @@
 import ts from 'rollup-plugin-typescript2';
 import terser from '@rollup/plugin-terser';
-import commonjs from "@rollup/plugin-commonjs";
 export default {
   input: 'src/index.ts',
   output: [
     {
       name: 'request-index',
       file: 'dist/index.js',
+      format: 'umd'
     }
   ],
   external: ['lodash-es', 'qs', '@brushes/utils', 'flyio/src/adapter/wx', 'flyio/dist/npm/engine-wrapper', 'flyio/dist/npm/fly'],
   plugins: [
     ts({}),
-    terser(),
-    commonjs({
-      exclude: 'node_modules/**',
-      include: ['src/**/*.ts', 'src/**/*.tsx']
-    }),
+    terser()
   ]
 };
